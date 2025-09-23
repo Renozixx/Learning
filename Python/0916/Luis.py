2b70861d75ddd648899a0ef0081311056682c9e0
Luis-Felipe13 <luisfelipeismaelsilvatte@gmail.com>
2025-09-23 10:33:21 -0300

    Conferindo erros

--- Python/0916/Luis.py
+++ Python/0916/Luis.py
@@ -5,6 +5,8 @@ import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
 import matplotlib.pyplot as plt
+from tensorflow.keras.callbacks import EarlyStopping
+import math
 
 # Leitura do dataset
 ds = pd.read_csv('Walmart_Sales.csv')
@@ -28,26 +30,34 @@ X[coluna_numerica] = aply_scale.fit_transform(X[coluna_numerica])
 # Divisão de dados para teste e treino
 X_treino, X_teste, y_treino, y_teste = train_test_split(X, y, test_size=0.2, random_state=42)
 
+#Criação da Rede Neural
 rn = keras.Sequential([
+    #Camada de entrada
     layers.Dense(units=128, activation='relu', input_shape=[X_treino.shape[1]]),
-    
+    layers.Dropout(0.2),
+    #Camada Oculta
     layers.Dense(units=64, activation='relu'),
-
+    layers.Dropout(0.2),
+    #Camada de Saída
     layers.Dense(units=1)
 ])
-
+#compilação da rede
 rn.compile(optimizer='adam', loss='mean_squared_error')
 
+early_stopping = EarlyStopping(monitor='val_loss', patience=10, restore_best_weights=True)
+#execução da rede neural
 result = rn.fit(
     X_treino,
     y_treino,
-    epochs = 100,
-    validation_split=0.2
+    epochs = 200,
+    validation_split=0.2,
+    callbacks=[early_stopping]
 )
-
+#Printando o histórico de erros
 loss_teste = rn.evaluate(X_teste, y_teste)
-print(f'Erro Quadrático Médio (MSE) no conjunto de teste: {loss_teste:.2f}')
+print(f'{loss_teste:.2f}')
 
+#plotagem/gráfico
 plt.figure(figsize=(10, 6))
 plt.plot(result.history['loss'], label='Erro de Treino')
 plt.plot(result.history['val_loss'], label='Erro de Validação')
@@ -57,3 +67,6 @@ plt.ylabel('Erro Quadrático Médio (MSE)')
 plt.legend()
 plt.grid(True)
 plt.show()
+
+r = math.sqrt(loss_teste)
+print(r)
