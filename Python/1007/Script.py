41eb151a40e77e02452b831ff3b79dc8d6327256
Luis-Felipe13 <luisfelipeismaelsilvatte@gmail.com>
2025-10-13 15:49:28 -0300

    N_features error

--- Python/1007/Script.py
+++ Python/1007/Script.py
@@ -6,8 +6,11 @@ from sklearn.feature_selection import SelectKBest, f_regression
 from sklearn.ensemble import RandomForestRegressor
 import matplotlib.pyplot as plt
 import seaborn as sns
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_absolute_error, mean_squared_error
+import numpy as np
 
-data = pd.read_csv('Python\\1007\\household_power_consumption.csv', sep=';', index_col=None)
+data = pd.read_csv('household_power_consumption.csv')
 
 print(data)
 
@@ -24,7 +27,19 @@ cols_object = [
     'Global_intensity',
     'Sub_metering_1',
     'Sub_metering_2',
+    'Sub_metering_3'
 ]
+target_column = 'Global_active_power'
+features = [col for col in data.columns if col not in ['Date', target_column] and data[col].dtype != 'object']
+
+X = data[features]
+y = data[target_column]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, shuffle=False) # Use shuffle=False para manter a ordem temporal
+
+print("Total de Colunas em X:", X.shape[1])
+print("Tipos de Dados em X:")
+print(X.dtypes)
 
 for col in cols_object:
     data[col] = pd.to_numeric(data[col], errors='coerce')
@@ -47,4 +62,14 @@ myPipeline = Pipeline([
     ('selector', SelectKBest(score_func=f_regression)),  
     ('model', RandomForestRegressor(random_state=42))
 ])
+myPipeline.set_params(selector__k=5)
+
+myPipeline.fit(X_train, y_train)
+y_pred_rf = myPipeline.predict(X_test)
+
+mae_rf = mean_absolute_error(y_test, y_pred_rf)
+rmse_rf = np.sqrt(mean_squared_error(y_test, y_pred_rf))
+
+print(f"Random Forest - MAE: {mae_rf:.4f}")
+print(f"Random Forest - RMSE: {rmse_rf:.4f}")
 
